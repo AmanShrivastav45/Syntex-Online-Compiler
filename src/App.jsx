83ce8cb199154e80baddbe336a987c521af97353
Aman-Shrivastav <amanshrivastav4501@gmail.com>
2024-07-13 11:55:42 +0530

    Integrated vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <Analytics>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Analytics>
     </Router>
   );
 }
